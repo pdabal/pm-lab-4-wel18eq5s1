4bce0091d13d120a6535ff3c6241bf8208307491
korneliusz Dudus <korneliusz.dudus@student.wat.edu.pl>
2020-06-02 13:15:12 +0200

    Wykonano zadanie 3.3.1 – analogRead

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,9 +1,18 @@
 #include <Arduino.h>
-
+#define ADC_RES_DEF (5.0f / 1024.0f)
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600);
+  analogReference(DEFAULT);
+  pinMode(A5, INPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  uint16_t digital = analogRead(A5);
+  float voltage = ADC_RES_DEF * digital;
+  Serial.print("A5 = ");
+  Serial.print(digital);
+  Serial.print(" \t V(5) = ");
+  Serial.print(voltage);
+  Serial.println(" [V]");
+  delay(1000);
 }
