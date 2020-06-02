7e199e13508ad4184b8be42f00cda7b9d310b74e
Michal Klosiewicz <michal.klosiewicz@student.wat.edu.pl>
2020-06-02 13:53:45 +0200

    Wykonano zadanie 3.3.2 - ADCsingleREAD

--- Zadanie-3-3/src/main.cpp
+++ Zadanie-3-3/src/main.cpp
@@ -1,19 +1,37 @@
 #include <Arduino.h>
-#define ADC_RES_DEF (5.0f / 1024.0f)
+#define ADC_RESOLUTION_DEFAULT (5.0f / 1024.0f)
 
-void setup() {
-  Serial.begin(9600);
-  analogReference(DEFAULT);
-  pinMode(A5, INPUT);
+uint16_t digital;
+float voltage;
+
+uint16_t ADCsingleREAD(uint8_t adcToUse){
+uint16_t ADCval;
+
+ADMUX = adcToUse;
+ADMUX |= (1 << REFS0);
+ADMUX &= ~(1 << ADLAR);
+ADCSRA |= (1 << ADPS2) | (1 << ADPS1) | (1 << ADPS0);
+ADCSRA |= (1 << ADEN);
+ADCSRA |= (1 << ADSC);
+while (ADCSRA & (1 << ADSC))
+;
+ADCval = ADCL;
+ADCval = (ADCH << 8) + ADCval;
+
+return ADCval;
 }
 
-void loop() {
-  uint16_t digital = analogRead(A5);
-  float voltage = ADC_RES_DEF * digital;
-  Serial.print("A5 = ");
-  Serial.print(digital);
-  Serial.print(" \t V(5) = ");
-  Serial.print(voltage);
-  Serial.print(" [V]");
-  delay(1000);
+void setup(){
+    Serial.begin(9600);
+}
+
+void loop(){
+    digital = ADCsingleREAD(0);
+    voltage = ADC_RESOLUTION_DEFAULT * digital;
+    Serial.print("A0 = ");
+    Serial.print(digital);
+    Serial.print(" \t V(0) = ");
+    Serial.print(voltage);
+    Serial.println(" [V]");
+    delay(1000);
 }
