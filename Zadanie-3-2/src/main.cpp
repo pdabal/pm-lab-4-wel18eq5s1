15c1c759aedc627d279c210f2b6320e6ffb6130e
Kordian <kordian.kucharek@student.wat.edu.pl>
2020-06-02 13:15:06 +0200

    Wykonano zadanie dodatkowe 3.2.4 – T/C1 CAPT”.

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -2,18 +2,16 @@
 #include <avr/interrupt.h>
 
 int main(void){
-   DDRC |= (1 << PORTC1);
-   DDRD &= ~(1 << DDD4);
-   PORTD |= (1 << PORTD4);
-   TIMSK0 |= (1 << TOIE0);
-   TCCR0B |= (1 << CS02) | (1 << CS01) | (1 << CS00);
-   sei();
-   while(1) {
-      if (TCNT0 > 64)
-        PORTC |= (1 << PORTC1);
-   }
-}
+  DDRD &= ~(1 << DDD5);
+  PORTD |= (1 << PORTD5);
+  TIMSK1 |= (1 << TOIE1);
+  TCCR1B |= (1 << CS12) | (1 << CS11) | (1 << CS10);
+  sei();
 
-ISR(TIMER0_OVF_vect) {
-   PORTC &= ~(1 << PORTC1);
+  while(1){
+
+}
+}
+ISR(TIMER1_OVF_vect){
+
 }
