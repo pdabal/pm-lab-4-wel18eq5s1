cd16d31808f63ea29528ab8a49e0671303ad1f67
AleksyPazura <aleksy.pazura@student.wat.edu.pl>
2020-06-02 12:22:29 +0200

    Wykonano zadanie 3.2.2 - T/C0 reg

--- Zadanie-3-2/src/main.cpp
+++ Zadanie-3-2/src/main.cpp
@@ -1,17 +1,20 @@
 #include <Arduino.h>
-#define PIN_R 11
-#define PIN_G 9
-#define PIN_B 10
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-void setup() {
-  pinMode(PIN_R, OUTPUT);
-  pinMode(PIN_G, OUTPUT);
-  pinMode(PIN_B, OUTPUT);
+int main(void){
+  DDRC |= (1 << PORTC1);
+  DDRD &= ~(1 <<DDD4);
+  PORTD |= (1 << PORTD4);
+  TIMSK0 |= (1 << TOIE0);
+  TCCR0B |= (1 << CS02) | (1 << CS01) | (1 << CS00);
+  sei();
+  while(1){
+    if(TCNT0 > 64)
+      PORTC |= (1 << PORTC1);
+  }
 }
 
-void loop() {
-  analogWrite(PIN_R, rand());
-  analogWrite(PIN_G, rand());
-  analogWrite(PIN_B, rand());
-  delay(1000);
+ISR(TIMER0_OVF_vect) {
+  PORTC &= ~(1 << PORTC1);
 }
